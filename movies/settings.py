679b485c7a13b2f151b593f41437ab23345aa5c9
saiak <akkenapallysai45@gmail.com>
2021-04-03 19:18:36 +0530

    'allow'

--- movies/settings.py
+++ movies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13o)b%c5mih3k#br(d+k4#7&hpj@ka*^wqj8-+6v5kdwnjwy%6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hbnmd.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
